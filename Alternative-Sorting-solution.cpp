720601f79297ed7397c30e8adadb0c65fc52bb9b
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-10-25 19:59:36 +0530

    Create Alternative-Sorting-solution.cpp

---
+++ Alternative-Sorting-solution.cpp
@@ -0,0 +1,77 @@
+#########################################################################################  Question  ###########################################################################################################
+Given an array arr of distinct integers. Rearrange the array in such a way that the first element is the largest and the second element is the smallest, the third element is the second largest and the fourth element is the second smallest, and so on.
+
+Examples:
+
+Input: arr[] = [7, 1, 2, 3, 4, 5, 6]
+Output: [7, 1, 6, 2, 5, 3, 4]
+Explanation: The first element is first maximum and second element is first minimum and so on.
+Input: arr[] = [1, 6, 9, 4, 3, 7, 8, 2]
+Output: [9, 1, 8, 2, 7, 3, 6, 4]
+Explanation: The first element is first maximum and second element is first minimum and so on.
+Expected Time Complexity: O(nlogn).
+Expected Auxiliary Space: O(n).
+
+Constraints:
+1 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 105
+
+
+
+##########################################################################################  Solution  ##########################################################################################################
+
+
+  //{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+     int n=arr.size();
+        if(n==1) return arr;
+        sort(arr.begin(), arr.end());
+        vector<int>ans(n);
+        int ind=n-1;
+        for(int i=0; i<n; i+=2){
+            ans[i]=arr[ind--];
+        }
+        ind=0;
+        for(int i=1; i<n; i+=2){
+            ans[i]=arr[ind++];
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.alternateSort(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
